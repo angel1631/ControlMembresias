a0e897ef2a138cb75477026b3b719a0fd13a25a9
Angel Guillen <angel1631@gmail.com>
2022-05-20 15:07:55 -0600

    Update CMEvento.js

--- modelos/CMEvento.js
+++ modelos/CMEvento.js
@@ -8,9 +8,11 @@ module.exports = function(sequelize, DataTypes){
 		paranoid: true,
 		referencia: {representante: ['titulo']},
 		classMethods:  { associate: function(models){ 
-								CMEvento.belongsTo(models.Usuario, {as: 'Creador', foreignKey: {allowNull: false}})
-							}},
-		seguridad: {
+			CMEvento.belongsTo(models.Usuario, {as: 'Creador', foreignKey: {allowNull: false}})
+		}},
+		multiples: [
+			{id: 'CMAsistenciaId', tipo: 'multiple', botonera: ['insertar'], pintar: [1,2,3,4], name: 'Agregar asistente', modelo_insertar: 'CMAsistencia', campo_padre: 'CMEventoId'},
+		],seguridad: {
 			1: 'CMEventoIns', 2: 'CMEventoAct', 3: 'CMEventoEli', 4: 'CMEventoBus' 
 		}
 	});
