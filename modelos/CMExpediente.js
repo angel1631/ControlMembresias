2514320aec4bd989a00988601100fa4028e37905
Angel Guillen <angel1631@gmail.com>
2022-05-20 15:40:17 -0600

    Update CMExpediente.js

--- modelos/CMExpediente.js
+++ modelos/CMExpediente.js
@@ -1,17 +1,19 @@
 module.exports = function(sequelize, DataTypes){
 	let CMExpediente = sequelize.define('CMExpediente', {
 		id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, pintar: [2,3,4], name: 'Identificador', tipo: 'number'},
-		fecha: {type: DataTypes.DATE, allowNull: false, pintar: [1,2,3,4], unique: true, name: 'Fecha de la asistencia', tipo: 'date'}
-    },{
+		archivo: {type: DataTypes.STRING, pintar: [1,2,3,4], orden: 3, name: 'Adjunto para expediente', tipo: 'archivo'}
+	},{
 		freezeTableName: true,
 		paranoid: true,
 		referencia: {representante: ['titulo']},
 		classMethods:  { associate: function(models){ 
 								CMExpediente.belongsTo(models.Usuario, {as: 'Creador', foreignKey: {allowNull: false}}),
-								CMExpediente.belongsTo(models.CMMiembro, {foreignKey: {allowNull: false}})
+								CMExpediente.belongsTo(models.CMMiembro, {foreignKey: {allowNull: false}}),
+								CMExpediente.belongsTo(models.CMTipoExpediente, {foreignKey: {allowNull:false}})
 							}}, 
 		relaciones: {
-			CMMiembroId: {pintar: [1,2,3,4], name: 'Miembros', tipo: 'select'},
+			CMMiembroId: {pintar: [1,2,3,4], orden: 1, name: 'Miembros', tipo: 'select'},
+			CMTipoExpedienteId: {pintar: [1,2,3,4], orden: 2, name: 'Tipo expediente', tipo: 'select'},
 		},
 		seguridad: {
 			1: 'CMExpedienteIns', 2: 'CMExpedienteAct', 3: 'CMExpedienteEli', 4: 'CMExpedienteBus' 
