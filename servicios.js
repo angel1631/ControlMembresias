e0bad8188a7b0b6ae4d1f39967b8388d544f367b
Angel Guillen <angel1631@gmail.com>
2022-05-26 22:31:42 -0600

    Update servicios.js

--- servicios.js
+++ servicios.js
@@ -4,7 +4,7 @@ let fn = {};
 
 fn.agregar_asistencia_miembro = async ({CMEventoId, CMMiembroId, CreadorId})=>{
     await seguridad.validar_acceso_v2({access: 'CMCapAsi', user: CreadorId});
-    let asistencia_previa = await modelos.CMAsistencia.findAll({WHERE: {CMEventoId, CMMiembroId}});
+    let asistencia_previa = await modelos.CMAsistencia.findAll({where: {CMEventoId, CMMiembroId}});
     if(asistencia_previa.length>0) throw "El miembro ya tiene su asistencia registrada al evento";
     await modelos.CMAsistencia.create({CMEventoId,CMMiembroId,CreadorId});
     return {cod:1,msj:'Todo Ok'};
