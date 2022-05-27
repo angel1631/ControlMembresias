cfb6cb808288c00997964d52f716037909aa005b
Angel Guillen <angel1631@gmail.com>
2022-05-26 22:24:31 -0600

    Update controlador.js

--- controlador.js
+++ controlador.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const seguridad = require('../Seguridad/servicios.js');
 const services = require('./servicios.js');
 
-router.post('/agregar_asistencia_miembro', (req,res,next)=>{
+router.post('/agregar_asistencia_miembro', async (req,res,next)=>{
     try{
         let valores = core_funciones.validar_values(req.body.values, req.user.id);
         let respuesta = await services.agregar_asistencia_miembro(valores);
